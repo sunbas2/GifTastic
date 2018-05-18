c4eeb0dfe6bb026adaa1dcd92543ebc2ae58ecf0
Stephanie Zeng <stephanie56@users.noreply.github.com>
2018-05-18 12:00:56 -0400

    Reviewed code and added comments

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,31 @@
 var topics = ["surf", "fly", "beach", "motorcycle", "ninjas"];
 
+/** SUGGESTIONS **/
+// 1. declare the queryURL variable globally
+var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=Y32hi09aY0Vg8T1gZEp3rjSzkS0OhHKd&tag=";
+
+// 2. Wrap your repetitive code into a function named generateGif 
+function generateGif(keyword) {
+    var gifURL = queryURL + keyword;
+    $.ajax({
+        url: gifURL,
+        method: "GET"
+    })
+    .then(function(response) {
+        var imageURL = response.data.image_original_url;
+        var image = $("<img>");
+        image.attr("src", imageURL);
+        image.attr("alt", keyword);
+        $("#images").prepend(image);
+    });
+    
+// 3. and now you can reuse your function generateGif for each button like this...
+    $("#surf").on("click", generateGif("surf"));
+    $("#fly").on("click", generateGif("fly"));
+    
+// 4. For the next step, you can try to refactor your code and see how to use a for loop to generate buttons dynamically :)
+    
+
 $("#surf").on("click", function(){
     var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=Y32hi09aY0Vg8T1gZEp3rjSzkS0OhHKd&tag=surf"
         
